8d36faed322348a9f2fcb6e8a4da48857d40049c
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-07-03 23:05:13 +0000

    Allmon allmetric schema prototype (for oracle) - adding calendar and time dimensions

--- trunk/src-db/create-allmon-schema-allmetric--oracle.sql
+++ trunk/src-db/create-allmon-schema-allmetric--oracle.sql
@@ -19,6 +19,7 @@ AppMet 	Host1 	AppInst1 	POJO 	Class1.method 	Class.method 	ExecTime 	TS 	T
 AppMet 	Host1 	AppInst1 	EJB 	EJB1.method 	? 	ExecTime 	TS 	T
 AppMet 	Host1 	AppInst1 	MDBExec 	MDB1 	? 	ExecTime 	TS 	T
 AppMet 	Host1 	AppInst1 	MDBWait 	MDB1 	? 	ExecTime 	TS 	T
+AppMet 	Host1 	AppInst1 	SLC ServStatus - NV TS T   
 Rep 	Host1 	RepServ1 	QueueLength 	Rep1 	? 	NV 	TS 	T
 Rep 	Host1 	RepServ1 	RepExec 	Rep1 	? 	ExecTime 	TS 	T
 Rep 	Host1 	RepServ1 	RepWait 	Rep1 	? 	ExecTime 	TS 	T
@@ -43,6 +44,8 @@ HW 	Machine1 	- 	Temp 	CPUTemp1 	- 	NV 	TS 	T
 
 -------------------------------------------------------------------------------------------------------------------------
 -- drop schema
+DROP SEQUENCE am_cal_seq;
+DROP SEQUENCE am_tim_seq;
 DROP SEQUENCE am_arf_seq;
 DROP SEQUENCE am_mty_seq;
 DROP SEQUENCE am_hst_seq;
@@ -55,6 +58,8 @@ DROP SEQUENCE am_src_seq;
 DROP TABLE am_metricsdata;
 
 -- drop dimensions
+DROP TABLE am_calendar;
+DROP TABLE am_time;
 DROP TABLE am_instance;
 DROP TABLE am_host;
 DROP TABLE am_resource;
@@ -81,6 +86,7 @@ CREATE TABLE am_metrictype (
   am_arf_id NUMBER(10) NOT NULL,
   metricname VARCHAR(20) NOT NULL,
   metriccode VARCHAR(6) NOT NULL,
+  unit VARCHAR(6) DEFAULT '-' NOT NULL,
   CONSTRAINT am_mty_pk PRIMARY KEY (am_mty_id) USING INDEX,
   CONSTRAINT am_mty_am_arf_fk1 FOREIGN KEY (am_arf_id) REFERENCES am_artifact(am_arf_id)
 );
@@ -111,6 +117,7 @@ CREATE TABLE am_resource (
   am_mty_id NUMBER(10) NOT NULL,
   resourcename VARCHAR(200) NOT NULL,
   resourcecode VARCHAR(10),
+  unit VARCHAR(6) DEFAULT '-' NOT NULL,
   CONSTRAINT am_rsc_pk PRIMARY KEY (am_rsc_id) USING INDEX,
   CONSTRAINT am_rsc_am_mty_fk1 FOREIGN KEY (am_mty_id) REFERENCES am_metrictype(am_mty_id)
 );
@@ -130,33 +137,64 @@ CREATE SEQUENCE am_src_seq MINVALUE 1 MAXVALUE 999999999999999 INCREMENT BY 1;
 CREATE UNIQUE INDEX am_src_uk1 ON am_source(am_mty_id, sourcename);
 CREATE UNIQUE INDEX am_src_uk2 ON am_source(sourcecode);
 
+CREATE TABLE am_calendar (
+  am_cal_id NUMBER(10) NOT NULL,
+  caldate DATE NOT NULL,
+  year NUMBER(4) NOT NULL,
+  month NUMBER(2) NOT NULL, 
+  day NUMBER(2) NOT NULL, 
+  week_day NUMBER(1) NOT NULL,
+  year_day NUMBER(3) NOT NULL,
+  quarter NUMBER(1) NOT NULL,
+  week_of_year NUMBER(2) NOT NULL,
+  CONSTRAINT am_cal_pk PRIMARY KEY (am_cal_id) USING INDEX
+);
+CREATE SEQUENCE am_cal_seq MINVALUE 1 MAXVALUE 999999999999999 INCREMENT BY 1;
+CREATE UNIQUE INDEX am_cal_uk1 ON am_calendar(year, month, day);
+CREATE UNIQUE INDEX am_cal_uk2 ON am_calendar(caldate);
+CREATE INDEX am_cal_idx1 ON am_calendar(week_day, year_day, quarter, week_of_year);
+
+CREATE TABLE am_time(
+  am_tim_id NUMBER(10) NOT NULL,
+  t DATE NOT NULL, 
+  hour NUMBER(2) NOT NULL,
+  minute NUMBER(2) NOT NULL,
+  CONSTRAINT am_tim_pk PRIMARY KEY (am_tim_id) USING INDEX
+);
+CREATE SEQUENCE am_tim_seq MINVALUE 1 MAXVALUE 999999999999999 INCREMENT BY 1;
+CREATE UNIQUE INDEX am_tim_uk1 ON am_time(hour, minute);
+
 -- create fact table 
 CREATE TABLE am_metricsdata (
   am_met_id NUMBER(10) NOT NULL,
   --am_arf_id NUMBER(10) NOT NULL, -- Artifact
-  am_mty_id NUMBER(10) NOT NULL, -- Metric Type
+  --am_mty_id NUMBER(10) NOT NULL, -- Metric Type
   am_ins_id NUMBER(10) NOT NULL, -- Instance	
   am_hst_id NUMBER(10) NOT NULL, -- Host
   am_rsc_id NUMBER(10) NOT NULL, -- Resource	
   am_src_id NUMBER(10) NOT NULL, -- Source	
+  am_cal_id NUMBER(10) NOT NULL, -- Calendar
+  am_tim_id NUMBER(10) NOT NULL, -- Time
   metricvalue NUMBER(13,3) NOT NULL, -- Metric	
-  ts DATE NOT NULL, -- Time Stamp
-  -- Calendar
-  -- Time
-  loadts DATE DEFAULT SYSDATE NOT NULL, -- Time Stamp
+  ts DATE NOT NULL, -- Time Stamp -- TODO review to delete
+  loadts DATE DEFAULT SYSDATE NOT NULL, -- Loading to the fact table Time Stamp
   CONSTRAINT am_met_pk PRIMARY KEY (am_met_id) USING INDEX,
-  CONSTRAINT am_met_am_mty_fk1 FOREIGN KEY (am_mty_id) REFERENCES am_metrictype(am_mty_id),
+  --CONSTRAINT am_met_am_mty_fk1 FOREIGN KEY (am_mty_id) REFERENCES am_metrictype(am_mty_id),
   CONSTRAINT am_met_am_ins_fk1 FOREIGN KEY (am_ins_id) REFERENCES am_instance(am_ins_id),
   CONSTRAINT am_met_am_hst_fk1 FOREIGN KEY (am_hst_id) REFERENCES am_host(am_hst_id),
   CONSTRAINT am_met_am_rsc_fk1 FOREIGN KEY (am_rsc_id) REFERENCES am_resource(am_rsc_id),
-  CONSTRAINT am_met_am_src_fk1 FOREIGN KEY (am_src_id) REFERENCES am_source(am_src_id)
+  CONSTRAINT am_met_am_src_fk1 FOREIGN KEY (am_src_id) REFERENCES am_source(am_src_id),
+  CONSTRAINT am_met_am_cal_fk1 FOREIGN KEY (am_cal_id) REFERENCES am_calendar(am_cal_id),
+  CONSTRAINT am_met_am_tim_fk1 FOREIGN KEY (am_tim_id) REFERENCES am_time(am_tim_id)
 );
 CREATE SEQUENCE am_met_seq MINVALUE 1 MAXVALUE 999999999999999 INCREMENT BY 1 CACHE 100;
-CREATE INDEX am_met_am_mty_idx1 ON am_metricsdata(am_mty_id);
+--CREATE INDEX am_met_am_mty_idx1 ON am_metricsdata(am_mty_id);
 CREATE INDEX am_met_am_ins_idx1 ON am_metricsdata(am_ins_id);
 CREATE INDEX am_met_am_hst_idx1 ON am_metricsdata(am_hst_id);
 CREATE INDEX am_met_am_rsc_idx1 ON am_metricsdata(am_rsc_id);
 CREATE INDEX am_met_am_src_idx1 ON am_metricsdata(am_src_id);
+CREATE INDEX am_met_am_cal_idx1 ON am_metricsdata(am_cal_id);
+CREATE INDEX am_met_am_tim_idx1 ON am_metricsdata(am_tim_id);
 
 
 -------------------------------------------------------------------------------------------------------------------------
@@ -182,27 +220,78 @@ WITH ones AS
 
 CREATE OR REPLACE VIEW vam_metricsdata AS
 SELECT ama.am_arf_id, ama.artifactname, ama.artifactcode, 
-       amt.am_mty_id, amt.metricname, amt.metriccode,
+       amt.am_mty_id, amt.metricname, amt.metriccode, amt.unit,
        ami.am_ins_id, ami.instancename, ami.instancecode, ami.url,
        amh.am_hst_id, amh.hostname, amh.hostcode, amh.hostip,
        amr.am_rsc_id, amr.resourcename, amr.resourcecode,
-       ams.am_src_id, ams.sourcename, ams.sourcecode
+       ams.am_src_id, ams.sourcename, ams.sourcecode,
+       amm.metricvalue, amm.ts, amm.loadts
 FROM  am_metricsdata amm, 
       am_metrictype amt, am_instance ami, am_host amh, am_resource amr, am_source ams, 
       am_artifact ama
-WHERE amm.am_mty_id = amt.am_mty_id
-AND   amm.am_ins_id = ami.am_ins_id
+WHERE amm.am_ins_id = ami.am_ins_id
 AND   amm.am_hst_id = amh.am_hst_id
 AND   amm.am_rsc_id = amr.am_rsc_id
 AND   amm.am_src_id = ams.am_src_id
 --
 AND   amt.am_arf_id = ama.am_arf_id
-AND   ami.am_arf_id = ama.am_arf_id -- redundant
-AND   amr.am_mty_id = amt.am_mty_id -- redundant
-AND   ams.am_mty_id = amt.am_mty_id; -- redundant
+AND   ami.am_arf_id = ama.am_arf_id
+AND   amr.am_mty_id = amt.am_mty_id
+AND   ams.am_mty_id = amt.am_mty_id;
 
+CREATE OR REPLACE VIEW vam_metricsdata_cal AS
+SELECT ama.am_arf_id, ama.artifactname, ama.artifactcode, 
+       amt.am_mty_id, amt.metricname, amt.metriccode, amt.unit,
+       ami.am_ins_id, ami.instancename, ami.instancecode, ami.url,
+       amh.am_hst_id, amh.hostname, amh.hostcode, amh.hostip,
+       amr.am_rsc_id, amr.resourcename, amr.resourcecode,
+       ams.am_src_id, ams.sourcename, ams.sourcecode,
+       amm.metricvalue, amm.ts, amm.loadts,
+       amc.year, amc.month, amc.day, amc.week_day, amc.year_day, amc.quarter, amc.week_of_year, amti.hour, amti.minute
+FROM  am_metricsdata amm, 
+      am_metrictype amt, am_instance ami, am_host amh, am_resource amr, am_source ams, 
+      am_artifact ama,
+      am_calendar amc, am_time amti
+WHERE amm.am_ins_id = ami.am_ins_id
+AND   amm.am_hst_id = amh.am_hst_id
+AND   amm.am_rsc_id = amr.am_rsc_id
+AND   amm.am_src_id = ams.am_src_id
+--
+AND   amt.am_arf_id = ama.am_arf_id
+AND   ami.am_arf_id = ama.am_arf_id
+AND   amr.am_mty_id = amt.am_mty_id
+AND   ams.am_mty_id = amt.am_mty_id
+-- 
+AND   amm.am_cal_id = amc.am_cal_id
+AND   amm.am_tim_id = amti.am_tim_id;
 
 -------------------------------------------------------------------------------------------------------------------------
+-- fill the static dimension - up to 2020
+INSERT INTO am_calendar(am_cal_id, caldate, YEAR, MONTH, DAY, week_day, year_day, quarter, week_of_year)
+  SELECT am_cal_seq.NEXTVAL,
+         caldate,
+         to_char(caldate, 'YYYY'),
+         to_char(caldate, 'MM'),
+         to_char(caldate, 'DD'),
+         to_char(caldate, 'D'),
+         to_char(caldate, 'DDD'),
+         to_char(caldate, 'Q'),
+         to_char(caldate, 'WW') --to_char(caldate, 'IW')
+  FROM   (SELECT to_date('2007-01-01', 'YYYY-MM-DD') + x AS caldate
+          FROM   pivot p
+          WHERE  p.x < 13 * 365.25 -- 0.25 - 1 day for every forth year
+          ORDER  BY 1);
+INSERT INTO am_time(am_tim_id, t, hour, minute)
+  SELECT am_tim_seq.NEXTVAL,
+         t,
+         to_char(t, 'HH24'),
+         to_char(t, 'MI')
+  FROM   (SELECT to_date('1970-01-01', 'YYYY-MM-DD') + x / (24*60)  AS t
+          FROM   pivot p
+          WHERE  p.x < 24*60
+          ORDER  BY 1);
+COMMIT;
+
 -- fill up default data for not dynamic dimensions
 -- OS, AppMet, Rep, JVM, DB, HW
 INSERT INTO am_artifact(am_arf_id, artifactname, artifactcode) VALUES(am_arf_seq.NEXTVAL, 'Operating System', 'OS'); 
@@ -213,10 +302,11 @@ INSERT INTO am_artifact(am_arf_id, artifactname, artifactcode) VALUES(am_arf_seq
 INSERT INTO am_artifact(am_arf_id, artifactname, artifactcode) VALUES(am_arf_seq.NEXTVAL, 'Hardware', 'HW'); 
 COMMIT;
 
-INSERT INTO am_instance(am_ins_id, am_arf_id, instancename, instancecode) VALUES(am_ins_seq.NEXTVAL, (SELECT aa.am_arf_id FROM am_artifact aa WHERE aa.artifactcode = 'APP'), 'Petstore', 'PETSTR'); 
+INSERT INTO am_metrictype(am_mty_id, am_arf_id, metricname, metriccode, unit) VALUES(am_mty_seq.NEXTVAL, (SELECT aa.am_arf_id FROM am_artifact aa WHERE aa.artifactcode = 'APP'), 'Struts Action Class', 'ACTCLS', 'ms'); 
+INSERT INTO am_metrictype(am_mty_id, am_arf_id, metricname, metriccode) VALUES(am_mty_seq.NEXTVAL, (SELECT aa.am_arf_id FROM am_artifact aa WHERE aa.artifactcode = 'APP'), 'Service Level Check', 'APPSLC'); 
 COMMIT;
 
-INSERT INTO am_metrictype(am_mty_id, am_arf_id, metricname, metriccode) VALUES(am_mty_seq.NEXTVAL, (SELECT aa.am_arf_id FROM am_artifact aa WHERE aa.artifactcode = 'APP'), 'Struts Action Class', 'ACTCLS'); 
+INSERT INTO am_instance(am_ins_id, am_arf_id, instancename, instancecode) VALUES(am_ins_seq.NEXTVAL, (SELECT aa.am_arf_id FROM am_artifact aa WHERE aa.artifactcode = 'APP'), 'Petstore', 'PETSTR'); 
 COMMIT;
 
 INSERT INTO am_host(am_hst_id, hostname, hostcode, hostip) VALUES(am_hst_seq.NEXTVAL, 'Example Host', 'EXPHST', '123.123.123.123'); 
@@ -225,11 +315,26 @@ COMMIT;
 -------------------------------------------------------------------------------------------------------------------------
 -- check data
 
+SELECT COUNT(*) FROM am_calendar;
+SELECT COUNT(*) FROM am_time;
+SELECT COUNT(*) FROM am_artifact;
+
 SELECT COUNT(*) FROM am_metricsdata;
 
 SELECT COUNT(*) FROM vam_metricsdata;
 
 
+SELECT vm.artifactname, vm.metricname, vm.unit, vm.resourcename, COUNT(*), AVG(vm.metricvalue)
+FROM vam_metricsdata vm 
+GROUP BY vm.artifactname, vm.metricname, vm.unit, vm.resourcename
+ORDER BY 1, 2, 3, 4;
 
+SELECT vm.artifactname, vm.hostname, vm.metricname, COUNT(*), AVG(vm.metricvalue)
+FROM vam_metricsdata vm 
+GROUP BY vm.artifactname, vm.hostname, vm.metricname
+ORDER BY 1, 2, 3;
 
-
+SELECT vmc.artifactname, vmc.metricname, vmc.unit, vmc.year, vmc.month, vmc.day, vmc.hour, COUNT(*), AVG(vmc.metricvalue)
+FROM vam_metricsdata_cal vmc
+GROUP BY vmc.artifactname, vmc.metricname, vmc.unit, vmc.year, vmc.month, vmc.day, vmc.hour
+ORDER BY 1, 2, 3, 4, 5, 6, 7;
---
+++ trunk/src-db/rnd/insert-test-data-allmetric--oracle-for-actions__fc_acition.sql
@@ -0,0 +1,148 @@
+
+-------------------------------------------------------------------------------------------------------------------------
+-- Action classes
+
+-- insert resources (action class) - very dynamic data
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename)
+SELECT am_rsc_seq.NEXTVAL,
+       (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'ACTCLS'),
+       dc.class_name
+FROM dm_class dc;
+COMMIT;
+
+-- insert sources (users) - very dynamic data
+INSERT INTO am_source(am_src_id, am_mty_id, sourcename)
+SELECT am_src_seq.NEXTVAL,
+       (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'ACTCLS'),
+       dt.tropics_user
+FROM dm_tropics_user dt;
+COMMIT;
+
+-- insert fact table 
+--SELECT * FROM fc_action fa WHERE fa.datetime > SYSDATE - 1
+INSERT INTO am_metricsdata(am_met_id, am_ins_id, am_hst_id, am_rsc_id, am_src_id, am_cal_id, am_tim_id, metricvalue, ts)
+SELECT am_met_seq.NEXTVAL, 
+       (SELECT ai.am_ins_id FROM am_instance ai WHERE ai.instancecode = 'PETSTR'),
+       (SELECT ah.am_hst_id FROM am_host ah WHERE ah.hostcode = 'EXPHST'),
+       ar.am_rsc_id,
+       s.am_src_id,
+       (SELECT ac.am_cal_id FROM am_calendar ac WHERE ac.caldate = trunc(fa.datetime, 'DD')),
+       (SELECT t.am_tim_id FROM am_time t WHERE t.hour = to_char(fa.datetime, 'HH24') AND t.minute = to_char(fa.datetime, 'MI')),
+       fa.exectime_ms,
+       fa.datetime 
+FROM  fc_action fa,  dm_class dc, am_resource ar, dm_tropics_user dt, am_source s
+WHERE fa.dm_cls_id = dc.dm_cls_id
+AND   dc.class_name = ar.resourcename
+AND   fa.dm_tru_id = dt.dm_tru_id
+AND   dt.tropics_user = s.sourcename
+AND   fa.datetime > SYSDATE - 1; --AND fa.datetime > SYSDATE - 10;
+COMMIT;
+
+-- check loaded data
+SELECT COUNT(*) FROM am_metricsdata;
+
+SELECT COUNT(*) FROM vam_metricsdata;
+
+SELECT vam.resourcename, vam.sourcename, vam.metricvalue, vam.ts
+FROM   vam_metricsdata vam
+WHERE  vam.artifactcode = 'APP'
+AND    vam.hostcode = 'EXPHST'
+AND    vam.instancecode = 'PETSTR'
+AND    vam.metriccode = 'ACTCLS';
+
+SELECT vm.resourcename, COUNT(*) FROM vam_metricsdata vm GROUP BY vm.resourcename;
+
+
+-------------------------------------------------------------------------------------------------------------------------
+-- Service level check
+
+-- insert resources (name of service check) - stable data
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'Service Status', 'SRVSTS');
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'No of logged-in users', 'LOGUSR');
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'No of stateful EJBs', 'SFEJBS');
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'Java calculation time - Web container', 'JAVWEB');
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'Java calculation time - EJB container', 'JAVEJB');
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'Database calculation time - Simple', 'DBSMPL');
+INSERT INTO am_resource(am_rsc_id, am_mty_id, resourcename, resourcecode) 
+VALUES(am_rsc_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'Database calculation time - Complex', 'DBCMPL');
+COMMIT;
+
+-- insert sources (no source) - stable data
+INSERT INTO am_source(am_src_id, am_mty_id, sourcename, sourcecode)
+VALUES(am_src_seq.NEXTVAL, (SELECT am.am_mty_id FROM am_metrictype am WHERE am.metriccode = 'APPSLC'), 'No source', 'APPNOS');
+COMMIT;
+
+-- insert fact table 
+INSERT INTO am_metricsdata(am_met_id, am_ins_id, am_hst_id, am_rsc_id, am_src_id, am_cal_id, am_tim_id, metricvalue, ts)
+SELECT am_met_seq.NEXTVAL, 
+       (SELECT ai.am_ins_id FROM am_instance ai WHERE ai.instancecode = 'PETSTR'),
+       (SELECT ah.am_hst_id FROM am_host ah WHERE ah.hostcode = 'EXPHST'),
+       (SELECT ar.am_rsc_id FROM am_resource ar WHERE ar.resourcecode = 'LOGUSR'),
+       (SELECT s.am_src_id FROM am_source s WHERE s.sourcecode = 'APPNOS'),
+       (SELECT ac.am_cal_id FROM am_calendar ac WHERE ac.caldate = trunc(vt.datetime, 'DD')),
+       (SELECT t.am_tim_id FROM am_time t WHERE t.hour = to_char(vt.datetime, 'HH24') AND t.minute = to_char(vt.datetime, 'MI')),
+       nvl(vt.tropics_users, 0),
+       vt.datetime 
+FROM  vmd_ex_tsc_all vt
+WHERE vt.datetime > SYSDATE - 1 -- AND vt.datetime > SYSDATE - 10
+AND   vt.tsc_host = 'toras51:7779';
+COMMIT;
+
+INSERT INTO am_metricsdata(am_met_id, am_ins_id, am_hst_id, am_rsc_id, am_src_id, am_cal_id, am_tim_id, metricvalue, ts)
+SELECT am_met_seq.NEXTVAL, 
+       (SELECT ai.am_ins_id FROM am_instance ai WHERE ai.instancecode = 'PETSTR'),
+       (SELECT ah.am_hst_id FROM am_host ah WHERE ah.hostcode = 'EXPHST'),
+       (SELECT ar.am_rsc_id FROM am_resource ar WHERE ar.resourcecode = 'SFEJBS'),
+       (SELECT s.am_src_id FROM am_source s WHERE s.sourcecode = 'APPNOS'),
+       (SELECT ac.am_cal_id FROM am_calendar ac WHERE ac.caldate = trunc(vt.datetime, 'DD')),
+       (SELECT t.am_tim_id FROM am_time t WHERE t.hour = to_char(vt.datetime, 'HH24') AND t.minute = to_char(vt.datetime, 'MI')),
+       nvl(vt.stateful_ejb_count, 0),
+       vt.datetime 
+FROM  vmd_ex_tsc_all vt
+WHERE vt.datetime > SYSDATE - 1 -- AND vt.datetime > SYSDATE - 10
+AND   vt.tsc_host = 'toras51:7779';
+COMMIT;
+
+INSERT INTO am_metricsdata(am_met_id, am_ins_id, am_hst_id, am_rsc_id, am_src_id, am_cal_id, am_tim_id, metricvalue, ts)
+SELECT am_met_seq.NEXTVAL, 
+       (SELECT ai.am_ins_id FROM am_instance ai WHERE ai.instancecode = 'PETSTR'),
+       (SELECT ah.am_hst_id FROM am_host ah WHERE ah.hostcode = 'EXPHST'),
+       (SELECT ar.am_rsc_id FROM am_resource ar WHERE ar.resourcecode = 'DBCMPL'),
+       (SELECT s.am_src_id FROM am_source s WHERE s.sourcecode = 'APPNOS'),
+       (SELECT ac.am_cal_id FROM am_calendar ac WHERE ac.caldate = trunc(vt.datetime, 'DD')),
+       (SELECT t.am_tim_id FROM am_time t WHERE t.hour = to_char(vt.datetime, 'HH24') AND t.minute = to_char(vt.datetime, 'MI')),
+       nvl(vt.db_complex_calc_time, 0),
+       vt.datetime 
+FROM  vmd_ex_tsc_all vt
+WHERE vt.datetime > SYSDATE - 1 -- AND vt.datetime > SYSDATE - 10
+AND   vt.tsc_host = 'toras51:7779';
+COMMIT;
+
+-- check loaded data
+SELECT COUNT(*) FROM vmd_ex_tsc_all;
+
+SELECT COUNT(*) FROM vam_metricsdata;
+
+SELECT vam.metricvalue, vam.ts
+FROM   vam_metricsdata vam
+WHERE  vam.artifactcode = 'APP'
+AND    vam.hostcode = 'EXPHST'
+AND    vam.instancecode = 'PETSTR'
+AND    vam.metriccode = 'APPSLC'
+AND    vam.resourcecode = 'LOGUSR' -- 'DBCMPL' 
+;
+
+
+-------------------------------------------------------------------------------------------------------------------------
+-- Report metrics
+
+
+
+
+
