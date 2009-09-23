39b2827e784138d1e37e8472fbf30c4d67aa9f5c
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-09-23 17:25:17 +0000

    Working on aggregating metrics on client (agent) side before sending packages to allmon server - solving null objects in the ready for loading messages queue problem

--- allmon-server/src/org/allmon/server/receiver/AggregatesReceiverMain.java
+++ allmon-server/src/org/allmon/server/receiver/AggregatesReceiverMain.java
@@ -7,20 +7,25 @@ import org.allmon.common.AllmonCommonConstants;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class AggregatesReceiverMain {
 
+    private static final Log logger = LogFactory.getLog(AggregatesReceiverMain.class);
+    
     public static void main(String args[]) throws Exception {
-        System.out.println("AggregatesReceiverMain>>>>");
+        logger.debug("begin");
         CamelContext context = new DefaultCamelContext();
         // Set up the ActiveMQ JMS Components
         ConnectionFactory connectionFactory = AllmonActiveMQConnectionFactory.server();
-        context.addComponent(AllmonCommonConstants.CLIENT_CAMEL_JMSQUEUE, JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        context.addComponent(AllmonCommonConstants.ALLMON_CAMEL_JMSQUEUE, JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
         context.addRoutes(new LoaderReceiverRouteBuilder());
         context.start();
         // ...
         //Thread.sleep(100 * 365 * 24 * 60 * 60 * 1000);
         //context.stop();
+        logger.debug("end");
     }
     
 }
