e5fcf8436a6357c6e8e419dd362f137bb1b2b94d
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-09-26 21:39:41 +0000

    Adding properties validating mechanism

--- trunk/allmon-client/src/org/allmon/common/AllmonPropertiesValidator.java
+++ trunk/allmon-client/src/org/allmon/common/AllmonPropertiesValidator.java
@@ -1,9 +1,26 @@
 package org.allmon.common;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class AllmonPropertiesValidator {
 
-    public void validate() {
-        
+    private static final Log logger = LogFactory.getLog(AllmonPropertiesValidator.class);
+    
+    public boolean validateMandatoryProperties() {
+        boolean validationResult = true;
+        String instance = AllmonPropertiesReader.getInstance().getValue(AllmonPropertiesConstants.ALLMON_CLIENT_INSTANCE_NAME);
+        if ("".equals(instance)) {
+            logger.fatal("Mandatory property " + AllmonPropertiesConstants.ALLMON_CLIENT_INSTANCE_NAME + " has not been speciefied in allmon.properties file");
+            validationResult = false;
+        }
+        return validationResult;
+    }
+    
+    public boolean validateSuggestedProperties() {
+        boolean validationResult = true;
+        // TODO add validation logic
+        return validationResult;
     }
     
 }
