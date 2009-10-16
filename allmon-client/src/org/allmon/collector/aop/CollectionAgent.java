bc79cbe95ba69cf525bffb4c3b9c2fa3eca26386
vnadgir <vnadgir@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-16 14:03:29 +0000

    Collector Module Basics

---
+++ allmon-client/src/org/allmon/collector/aop/CollectionAgent.java
@@ -0,0 +1,27 @@
+package org.allmon.collector.aop;
+
+
+
+public class CollectionAgent
+{	
+
+	public void startExecution(String resourceIdentifier)
+	{
+		
+	}
+	
+	public void endExecution(String resourceIdentifier)
+	{
+
+	}
+	
+	public void exceptionThrown(String resourceIdentifier)
+	{
+
+	}
+	
+	public void otherExceptionalConditions(String resourceIdentifier)
+	{
+
+	}
+}
