966b1555ea797cf9e56da09bb1ba1f6d651e99dd
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-21 17:49:50 +0000

    ALM-1 Working on allmon agents API

--- allmon-client-test/src/org/allmon/client/agent/HttpUrlCallAgentTest.java
+++ allmon-client-test/src/org/allmon/client/agent/HttpUrlCallAgentTest.java
@@ -7,9 +7,13 @@ public class HttpUrlCallAgentTest extends TestCase {
     public void testExecute() {
         HttpUrlCallAgent agent = new HttpUrlCallAgent();
         agent.setParameters(new String[]{
+                //"http://www.google.com/#hl=en&q=qwerty",
                 "http://lontd01/AdminConsole/statuscheck/default.aspx/CheckStatus", 
-                "\\w\\w\\w\\w",
+                "\\d\\d\\d\\d",
+                //"text/html",
+                //"application/x-www-form-urlencoded",
                 "application/json; charset=utf-8",
+                //""
                 "{ 'componentChecker': 'TTC.iTropics.ComponentCheckers.TropicsDawsComponentChecker, TTC.iTropics.ComponentCheckers' }"
             });
         agent.execute();
