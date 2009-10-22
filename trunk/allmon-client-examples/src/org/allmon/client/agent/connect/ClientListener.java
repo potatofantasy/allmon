45c59d66d48dea80eb763cbf8c490744b5d64f2d
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-22 21:06:50 +0000

    Adding more JMX examples

---
+++ trunk/allmon-client-examples/src/org/allmon/client/agent/connect/ClientListener.java
@@ -0,0 +1,10 @@
+package org.allmon.client.agent.connect;
+
+import javax.management.Notification;
+import javax.management.NotificationListener;
+
+public class ClientListener implements NotificationListener {
+	public void handleNotification(Notification notification, Object handback) {
+		System.out.println("\nReceived notification: " + notification);
+	}
+}
