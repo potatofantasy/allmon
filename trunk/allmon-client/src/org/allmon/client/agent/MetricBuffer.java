64edccffa407be46a34a56525d46345d6e87f46b
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-25 16:20:09 +0000

    ALM-2 Adding a new MetricsSender implementation with JMS broker service service check.

--- trunk/allmon-client/src/org/allmon/client/agent/MetricBuffer.java
+++ trunk/allmon-client/src/org/allmon/client/agent/MetricBuffer.java
@@ -1,6 +1,7 @@
 package org.allmon.client.agent;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -46,49 +47,86 @@ public class MetricBuffer {
         
     private class BufferingThread<T> extends Thread {
         
-        //private List<E> buffer = Collections.synchronizedList(new ArrayList<E>()); //new ArrayList<E>();
+        //private List<T> buffer = Collections.synchronizedList(new ArrayList<T>()); // Slow!
     	private List<T> buffer = new ArrayList<T>();
-        private long flushCount = 0;
         private long flushingInterval = 10000;
         
+        private long flushCount = 0;
+        private long flushedItemsCount = 0;
+        private long lastFlushTime = 0;
+        private long summaryFlushTime = 0;
+        private long lastSendTime = 0;
+        private long summarySendTime = 0;
+        
         public final void run() {
-            logger.debug("run run run and buffer ...");
+            logger.debug("run run run and keep buffering ...");
             while (true) {
                 try {
                     Thread.sleep(flushingInterval);
                 } catch (InterruptedException e) {
                 	logger.error(e.getMessage(), e);
                 }
-                logger.debug("flushing buffer ...");
                 flush();
             }
         }
         
-        private void flush() {
+        private synchronized void flush() {
         	logger.debug("flushing " + (flushCount + 1) + " starts...");
-            Iterator<T> iterator = buffer.iterator();
-            while(iterator.hasNext()) {
-            	synchronized (buffer) {
-            		T t = iterator.next();
-            		logger.debug("> " + t); // TODO this logging must be deleted
-	                iterator.remove();
-            	}
+        	
+            List<T> flushingBuffer = new ArrayList<T>();
+            
+            // coping data to flushing buffer
+            long t0 = System.currentTimeMillis(); //System.nanoTime();
+        	int copiedCount = 0;
+            synchronized (buffer) {
+                // because no other than this thread can add or remove items from buffer list,
+                // we can check how many items we have in the list and move them to the flushing buffer backwards,
+                // so the synchronized block in add method is not needed and other items can be still added to the list
+            	
+            	//very slow!
+            	//Iterator<T> iterator = buffer.iterator();
+            	//while(iterator.hasNext()) {
+            	//	T t = iterator.next();
+            	//	iterator.remove();
+	                        	
+            	for (int i = buffer.size() - 1; i >= 0; i--) {
+            	    T t = buffer.get(i);
+				    buffer.remove(i);
+                    
+	                flushingBuffer.add(t);
+	                copiedCount++;
+	                //logger.debug("flushing - removing item from buffer > " + t); // TODO this logging must be deleted
+	        	}
             }
+            lastFlushTime = System.currentTimeMillis() - t0; //System.nanoTime();
+            
+        	logger.debug("copied items: " + copiedCount + " in: " + lastFlushTime + "ms, flushingBuffer is ready to send data and clear");
+        	
+            // TODO send collected in flushingBuffer data 
+            // TODO if sending operation is to long reevaluate creating helper thread to send this data, letting main buffering thread continue
+            flushingBuffer.clear();
             
             flushCount++;
-            logger.debug("end of flushing");
+            flushedItemsCount += copiedCount;
+            summaryFlushTime += lastFlushTime;
+            
+            logger.debug("summary of metric messages flushed: " + flushedItemsCount + " items, flushingBuffer is ready to send data and clear");
+        	logger.debug("flushing end.");
         }
         
         private void add(T t) {
+        	// XXX STOP THE WORLD!
+        	// adding is not permitted when flushing method is copying data to flushing buffer ready to send
         	synchronized (buffer) {
-        		buffer.add(t);
+        		//logger.debug("adding item to buffer> " + t); // TODO this logging must be deleted
+            	buffer.add(t);
         	}
         }
         
     }
     
     /**
-     * Add metric object to buffer ready to send.
+     * Add metric message object to buffer ready to send.
      */
     public void add(MetricMessage metricMessage) {
         bufferingThread.add(metricMessage);
@@ -106,8 +144,17 @@ public class MetricBuffer {
     	return bufferingThread.flushCount;
     }
     
+    public long getFlushedItemsCount() {
+    	return bufferingThread.flushedItemsCount;
+    }
+    
+    public long getSummaryFlushTime() {
+    	return bufferingThread.summaryFlushTime;
+    }
+    
     /**
-     * Metric buffer flushing interval  
+     * Sets metrics buffer flushing interval (time between executing flush method).
+     * 
      * @param flushingInterval
      */
     public void setFlushingInterval(long flushingInterval) {
