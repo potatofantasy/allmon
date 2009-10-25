2a56de94de2436caeba23d1af70239d5ffe2d3e4
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-25 16:20:18 +0000

    ALM-2 Adding a new MetricsSender implementation with JMS broker service service check.

--- trunk/allmon-client-test/src/org/allmon/client/agent/MetricBufferTest.java
+++ trunk/allmon-client-test/src/org/allmon/client/agent/MetricBufferTest.java
@@ -3,6 +3,7 @@ package org.allmon.client.agent;
 import junit.framework.TestCase;
 
 import org.allmon.common.AllmonPropertiesReader;
+import org.allmon.common.MetricMessage;
 import org.allmon.common.MetricMessageFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,27 +19,29 @@ public class MetricBufferTest extends TestCase {
     public void test() {
         // first invocation creates and starts buffering process
         MetricBuffer.getInstance();
+        MetricBuffer.getInstance().setFlushingInterval(1000);
+        
         logger.debug("buffer is running in background...");
         
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
+        MetricBuffer.getInstance().add(createMessage());
+        MetricBuffer.getInstance().add(createMessage());
+        MetricBuffer.getInstance().add(createMessage());
         
         logger.debug("end of adding.");
         logger.debug("now wait...");
         
-        sleep();
+        sleep(1500);
         
         logger.debug("add something aggain");
         
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
-        MetricBuffer.getInstance().add(MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000)));
+        MetricBuffer.getInstance().add(createMessage());
+        MetricBuffer.getInstance().add(createMessage());
+        MetricBuffer.getInstance().add(createMessage());
+        MetricBuffer.getInstance().add(createMessage());
         
         logger.debug("and wait...");
         
-        sleep();
+        sleep(1500);
         
         MetricBuffer.getInstance();
         
@@ -46,13 +49,48 @@ public class MetricBufferTest extends TestCase {
         
     }
     
-    private static void sleep() {
+    public void testConcurent() {
+    	// first invocation creates and starts buffering process
+        MetricBuffer.getInstance();
+        logger.debug("buffer is running in background...");
+    	
+        MetricBuffer.getInstance().setFlushingInterval(1000);
+        sleep(990);
+        
+        Thread t = new Thread(new Runnable() {
+			public void run() {
+				createMessageAndAddToBuffer(100, 1);				
+			}
+        });
+        t.start();
+        
+        createMessageAndAddToBuffer(100, 1);
+        
+        sleep(1500);
+        
+    }
+    
+    private static void sleep(long sleep) {
         try {
-            Thread.sleep(15000);
+            Thread.sleep(sleep);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+    
+    private static MetricMessage createMessage() {
+    	return MetricMessageFactory.createClassMessage("class1", "m1", "classX", "mX", (long)(Math.random() * 1000));
+    }
+    
+    private static void createMessageAndAddToBuffer(int c, long sleep) {
+    	for (int i = 0; i < c; i++) {
+            MetricBuffer.getInstance().add(createMessage());
+            if (sleep > 0) {
+            	sleep(sleep);
+            }
+		}
+    }
+    
     
 }
