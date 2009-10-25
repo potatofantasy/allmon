6750e4a911f166c40ac828d94ff9fa5974371efc
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-25 23:11:51 +0000

    ALM-2 Adding a new MetricsSender implementation with JMS broker service service check.

--- allmon-client-test/src/org/allmon/client/agent/JmsBrokerHealthSamplerTest.java
+++ allmon-client-test/src/org/allmon/client/agent/JmsBrokerHealthSamplerTest.java
@@ -2,19 +2,19 @@ package org.allmon.client.agent;
 
 import junit.framework.TestCase;
 
-public class JmsBrokerSamplerTest extends TestCase {
+public class JmsBrokerHealthSamplerTest extends TestCase {
 
     public void testSamplingIsUp() {
-        boolean isUp = JmsBrokerSampler.getInstance().isJmsBrokerUp();
+        boolean isUp = JmsBrokerHealthSampler.getInstance().isJmsBrokerUp();
         assertTrue(isUp);
     }
     
     public void testSamplingEveryMinute() throws InterruptedException {
-        boolean isUp = JmsBrokerSampler.getInstance().isJmsBrokerUp();
-        long sampleTime0 = JmsBrokerSampler.getInstance().getLastCheckTime();
+        boolean isUp = JmsBrokerHealthSampler.getInstance().isJmsBrokerUp();
+        long sampleTime0 = JmsBrokerHealthSampler.getInstance().getLastCheckTime();
         assertTrue(isUp);
         Thread.sleep(150000);
-        long sampleTime1 = JmsBrokerSampler.getInstance().getLastCheckTime();
+        long sampleTime1 = JmsBrokerHealthSampler.getInstance().getLastCheckTime();
         assertTrue("Sample has to be taken every 60 sec", sampleTime1 - sampleTime0 <= 61000);
     }
     
