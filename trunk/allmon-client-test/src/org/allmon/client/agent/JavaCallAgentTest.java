0a48eb1b52b46d5da13cf75d1bcc01d47759a894
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-27 12:28:05 +0000

    ALM-2 Adding a new MetricsSender implementation with JMS broker service service check.

--- trunk/allmon-client-test/src/org/allmon/client/agent/JavaCallAgentTest.java
+++ trunk/allmon-client-test/src/org/allmon/client/agent/JavaCallAgentTest.java
@@ -14,7 +14,10 @@ public class JavaCallAgentTest extends TestCase {
 		
 		JavaCallAgent agent = new JavaCallAgent(message);
 		agent.entryPoint();
+        Thread.sleep(1500);
 		agent.exitPoint();
+		
+		Thread.sleep(10000);
 		
 	}
 	
