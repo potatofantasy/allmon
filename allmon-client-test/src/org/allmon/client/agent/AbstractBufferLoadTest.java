3af8012446bd0697b67d835101c502f116eb931e
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-28 16:18:53 +0000

    ALM-2 Adding a new MetricsSender implementation with JMS broker service service check.

---
+++ allmon-client-test/src/org/allmon/client/agent/AbstractBufferLoadTest.java
@@ -0,0 +1,60 @@
+package org.allmon.client.agent;
+
+import java.util.List;
+
+import org.allmon.common.AbstractLoadTest;
+import org.allmon.common.AllmonPropertiesReader;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class AbstractBufferLoadTest extends AbstractLoadTest<AbstractMetricBuffer<String>, Object> {
+
+    static {
+        AllmonPropertiesReader.readLog4jProperties();
+    }
+    
+    private static final Log logger = LogFactory.getLog(MetricBufferLoadTest.class);
+    
+    // stress test
+    private final static long THREADS_COUNT = 30;
+    private final static long STARTING_TIME_MILLIS = 1 * 1000;
+    private final static long SUBSEQUENT_CALLS_IN_THREAD_SLEEP_MAX = 0; // no sleep
+    private final static long SUBSEQUENT_CALLS_IN_THREAD = 10000;
+    
+    private final AbstractMetricBuffer<String> metricBuffer = new AbstractMetricBuffer<String>() {
+        public void send(List<String> flushingList) {
+            logger.debug("Sending items: " + flushingList.size());
+        }
+    };
+    
+    public void testMain() throws InterruptedException {
+        runLoadTest(THREADS_COUNT, 
+                STARTING_TIME_MILLIS, SUBSEQUENT_CALLS_IN_THREAD_SLEEP_MAX, 
+                SUBSEQUENT_CALLS_IN_THREAD, 3000);
+        
+        // force flushing
+        metricBuffer.flush();
+        
+        long expected = THREADS_COUNT * SUBSEQUENT_CALLS_IN_THREAD;
+        long actual = metricBuffer.getFlushedItemsCount();
+        assertEquals(expected, actual);
+        
+        logger.info("Total flushing time (handy for tuning FlushingInterval): " + metricBuffer.getSummaryFlushTime());
+    }
+    
+    public AbstractMetricBuffer<String> initialize() {
+        metricBuffer.setFlushingInterval(500);
+        logger.debug("buffer is running in background...");
+        return metricBuffer;
+    }
+    
+    public Object preCall(int iteration, AbstractMetricBuffer<String> metricBuffer) {
+        String string = "string" + iteration; 
+        metricBuffer.add(string);                     
+        return null;
+    }
+    
+    public void postCall(Object preCallParameters) {
+    }
+    
+}
