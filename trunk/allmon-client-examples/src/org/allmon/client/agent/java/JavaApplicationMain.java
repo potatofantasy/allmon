58d46d2d5d43b55126d645644eda73e02887addf
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-10-28 16:18:33 +0000

    ALM-2 Adding a new MetricsSender implementation with JMS broker service service check.

---
+++ trunk/allmon-client-examples/src/org/allmon/client/agent/java/JavaApplicationMain.java
@@ -0,0 +1,40 @@
+package org.allmon.client.agent.java;
+
+import java.io.IOException;
+
+import org.allmon.client.agent.JavaCallAgent;
+import org.allmon.common.MetricMessage;
+import org.allmon.common.MetricMessageFactory;
+
+public class JavaApplicationMain {
+
+    public static void main(String[] args) throws IOException {
+        
+        for (int i = 0; i < 5; i++) {
+            
+            System.out.println("Press a button...");
+            System.in.read();
+            
+            MetricMessage message = MetricMessageFactory.createClassMessage(
+                    JavaApplicationMain.class.getName(), "main", "ClassB", "methodB", 1);
+            
+            JavaCallAgent agent = new JavaCallAgent(message);
+            agent.entryPoint();
+            
+            // emulating some computations
+            try {
+                Thread.sleep((long)(Math.random() * 2000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                agent.exitPoint();
+            }
+            
+        }
+
+        System.out.println("Five messages after users interacins have been sent.");
+        System.out.println("End.");
+        
+    }
+
+}
