5627a0e16bbf4e99205cc01044257decf313a04e
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-01 19:05:53 +0000

    Working on agents

--- trunk/allmon-client-examples/src/org/allmon/client/agent/java/JavaApplicationMain.java
+++ trunk/allmon-client-examples/src/org/allmon/client/agent/java/JavaApplicationMain.java
@@ -2,47 +2,41 @@ package org.allmon.client.agent.java;
 
 import java.io.IOException;
 
-import org.allmon.client.agent.HttpUrlCallAgent;
+import org.allmon.client.agent.AgentContext;
 import org.allmon.client.agent.JavaCallAgent;
-import org.allmon.client.agent.MessageSender;
 import org.allmon.common.MetricMessage;
 import org.allmon.common.MetricMessageFactory;
 
 public class JavaApplicationMain {
 
     public static void main(String[] args) throws IOException {
-        
-    	for (int i = 0; i < 5; i++) {
-            
-            System.out.println("Press a button...");
-            //System.in.read();
-            
-            MetricMessage message = MetricMessageFactory.createClassMessage(
-                    JavaApplicationMain.class.getName(), "main", "ClassB", "methodB", 1);
-            
-            JavaCallAgent agent = new JavaCallAgent(message);
-            agent.entryPoint();
-            
-            // emulating some computations
-            try {
-                Thread.sleep((long)(Math.random() * 1000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                agent.exitPoint();
-            }
-            
-        }
-
-        System.out.println("Five messages after users interacins have been sent.");
-
-        // kill buffering thread
-        JavaCallAgent.getMetricBuffer().flushAndTerminate();
-        
+    	AgentContext agentContext = AgentContext.getInstance();
+    	try {
+	    	for (int i = 0; i < 5; i++) {
+	            System.out.println("Press a button...");
+	            //System.in.read();
+	            
+	            MetricMessage message = MetricMessageFactory.createClassMessage(
+	                    JavaApplicationMain.class.getName(), "main", "ClassB", "methodB", 1);
+	            
+	            JavaCallAgent agent = new JavaCallAgent(agentContext, message);
+	            agent.entryPoint();
+	            
+	            // emulating some computations
+	            try {
+	                Thread.sleep((long)(Math.random() * 1000));
+	            } catch (InterruptedException e) {
+	                e.printStackTrace();
+	            } finally {
+	                agent.exitPoint();
+	            }
+	        }
+	        System.out.println("Five messages after users interacins have been sent.");
+    	} finally {
+	        // kill buffering thread and close connections to broker
+	        agentContext.stop();
+    	}
         System.out.println("end.");
-        
-        // stopping conection pool to broker
-        MessageSender.stop();
         
     }
     
