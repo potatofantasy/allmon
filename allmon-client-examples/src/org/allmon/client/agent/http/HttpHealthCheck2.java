4c6dfacb325bb71d834a85f1e1d82390b889271c
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-01 19:11:20 +0000

    Working on agents

--- allmon-client-examples/src/org/allmon/client/agent/http/HttpHealthCheck2.java
+++ allmon-client-examples/src/org/allmon/client/agent/http/HttpHealthCheck2.java
@@ -1,30 +1,32 @@
 package org.allmon.client.agent.http;
 
+import org.allmon.client.agent.AgentContext;
 import org.allmon.client.agent.HttpUrlCallAgent;
 import org.allmon.client.agent.HttpUrlCallAgentBooleanStrategy;
 
 public class HttpHealthCheck2 {
 
 	public static void main(String[] args) throws InterruptedException {
-        
-        HttpUrlCallAgent agent = new HttpUrlCallAgent();
-        agent.setStrategy(new HttpUrlCallAgentBooleanStrategy());
-        agent.setParameters(new String[]{
-                "http://localhost:8161/", //"http://www.google.com/#hl=en&q=qwerty",
-                "\\d\\d",
-                "text/html", //"application/x-www-form-urlencoded",
-                ""
-            });
-        try {
-            agent.execute();
-        } catch (Exception e) {
-        }
-        
-        Thread.sleep(2000);
-        
-        // kill buffering thread
-        HttpUrlCallAgent.getMetricBuffer().flushAndTerminate();
-        
+		AgentContext agentContext = AgentContext.getInstance();
+    	try {
+		    HttpUrlCallAgent agent = new HttpUrlCallAgent(agentContext);
+		    agent.setStrategy(new HttpUrlCallAgentBooleanStrategy());
+	        agent.setParameters(new String[]{
+	                "http://localhost:8161/", //"http://www.google.com/#hl=en&q=qwerty",
+	                "\\d\\d",
+	                "text/html", //"application/x-www-form-urlencoded",
+	                ""
+	            });
+	        try {
+	            agent.execute();
+	        } catch (Exception e) {
+	        }
+	        
+	        Thread.sleep(2000);
+    	} finally {
+	        // kill buffering thread and close connections to broker
+	        agentContext.stop();
+    	}
         System.out.println("end.");
 
 	}
