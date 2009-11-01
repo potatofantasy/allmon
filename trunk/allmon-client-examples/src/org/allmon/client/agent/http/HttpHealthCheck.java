72e1d7d2cca33c6de6cb6f15d80dc1b4a5a45183
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-01 19:11:20 +0000

    Working on agents

--- trunk/allmon-client-examples/src/org/allmon/client/agent/http/HttpHealthCheck.java
+++ trunk/allmon-client-examples/src/org/allmon/client/agent/http/HttpHealthCheck.java
@@ -1,28 +1,30 @@
 package org.allmon.client.agent.http;
 
+import org.allmon.client.agent.AgentContext;
 import org.allmon.client.agent.HttpUrlCallAgent;
 import org.allmon.client.agent.HttpUrlCallAgentBooleanStrategy;
 
 public class HttpHealthCheck {
 
     public static void main(String[] args) throws InterruptedException {
-        
-        HttpUrlCallAgent agent = new HttpUrlCallAgent();
-        agent.setStrategy(new HttpUrlCallAgentBooleanStrategy());
-        agent.setParameters(new String[]{
-                //"http://www.google.com/#hl=en&q=qwerty",
-                "http://lontd01/AdminConsole/statuscheck/default.aspx/CheckStatus", 
-                "\"Success\":true",
-                "application/json; charset=utf-8", //"text/html", //"application/x-www-form-urlencoded",
-                "{ 'componentChecker': 'TTC.iTropics.ComponentCheckers.ITropicsServiceComponentChecker, TTC.iTropics.ComponentCheckers' }"
-            });
-        agent.execute();
-        
-        Thread.sleep(2000);
-        
-        // kill thread
-        HttpUrlCallAgent.getMetricBuffer().flushAndTerminate();
-        
+    	AgentContext agentContext = AgentContext.getInstance();
+    	try {
+		    HttpUrlCallAgent agent = new HttpUrlCallAgent(agentContext);
+	        agent.setStrategy(new HttpUrlCallAgentBooleanStrategy());
+	        agent.setParameters(new String[]{
+	                //"http://www.google.com/#hl=en&q=qwerty",
+	                "http://lontd01/AdminConsole/statuscheck/default.aspx/CheckStatus", 
+	                "\"Success\":true",
+	                "application/json; charset=utf-8", //"text/html", //"application/x-www-form-urlencoded",
+	                "{ 'componentChecker': 'TTC.iTropics.ComponentCheckers.ITropicsServiceComponentChecker, TTC.iTropics.ComponentCheckers' }"
+	            });
+	        agent.execute();
+	        
+	        Thread.sleep(2000);
+    	} finally {
+	        // kill buffering thread and close connections to broker
+	        agentContext.stop();
+    	}
         System.out.println("end.");
     }
     
