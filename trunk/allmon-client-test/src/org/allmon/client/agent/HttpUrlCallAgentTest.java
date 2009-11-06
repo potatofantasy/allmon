c6f03328fd22e65f04bb58eb8e007035bfa3e263
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-06 15:58:31 +0000

    Working on agents

--- trunk/allmon-client-test/src/org/allmon/client/agent/HttpUrlCallAgentTest.java
+++ trunk/allmon-client-test/src/org/allmon/client/agent/HttpUrlCallAgentTest.java
@@ -4,21 +4,43 @@ import junit.framework.TestCase;
 
 public class HttpUrlCallAgentTest extends TestCase {
 
-    public void testExecute() throws InterruptedException {
+    public void testCallWikipedia() throws InterruptedException {
         AgentContext agentContext = new AgentContext();
         try {
             HttpUrlCallAgent agent = new HttpUrlCallAgent(agentContext);
             agent.setStrategy(new HttpUrlCallAgentBooleanStrategy());
             agent.setParameters(new String[]{
+                    "org.allmon.client.agent.HttpUrlCallAgentBooleanStrategy",
+                    "http://www.wikipedia.org",
+                    "wikipedia",
+                    "text/html",
+                    "-",
+                    "www.wikipedia.com",
+                    "www.wikipedia.com-Checker", 
+                    "true"});
+            agent.execute();
+            Thread.sleep(2000);
+        } finally {
+            agentContext.stop();
+        }
+    }
+    
+    public void testCallGoogle() throws InterruptedException {
+        AgentContext agentContext = new AgentContext();
+        try {
+            HttpUrlCallAgent agent = new HttpUrlCallAgent(agentContext);
+            agent.setStrategy(new HttpUrlCallAgentBooleanStrategy());
+            agent.setParameters(new String[]{
+                    "org.allmon.client.agent.HttpUrlCallAgentBooleanStrategy",
                     "http://www.google.com",
                     "html",
-                    "text/html", 
-                    "0",
-                    "www.google.com", 
-                    "www.google.com-Checker"});
+                    "text/html",
+                    "-",
+                    "www.google.com",
+                    "www.google.com-Checker",
+                    "true"});
             agent.execute();
-        
-            Thread.sleep(5000);
+            Thread.sleep(2000);
         } finally {
             agentContext.stop();
         }
