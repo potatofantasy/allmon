890b101bfbd2253b44ea918b6c169d80b1388434
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-09 18:03:48 +0000

    Adding batch files running allmon server-side components

---
+++ allmon-server/run-receiver-loader.bat
@@ -0,0 +1,4 @@
+call set-classpath.bat
+
+java -Xmx512m -cp %CLASS_PATH% org.allmon.server.receiver.AggregatesReceiverMain
+
---
+++ allmon-server/set-homes.bat
@@ -0,0 +1,3 @@
+set ACTIVEMQ_HOME=c:\apache-activemq-5.2.0\
+set JAVA_HOME=c:\jdk1.5.0_12\
+rem c:\ant162\bin\ant
