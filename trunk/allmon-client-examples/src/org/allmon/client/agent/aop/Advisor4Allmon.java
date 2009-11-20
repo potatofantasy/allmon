3861b204fd0b17577990efa368c5d8d8dbf00104
vnadgir <vnadgir@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-20 11:05:24 +0000

    Controller should not know Agent. But we need to address issue of Passive Agent mapping to discrete Advisors. Need to explore how it integrates with multi-threading since Java Call Agent uses a Buffering Thread

--- trunk/allmon-client-examples/src/org/allmon/client/agent/aop/Advisor4Allmon.java
+++ trunk/allmon-client-examples/src/org/allmon/client/agent/aop/Advisor4Allmon.java
@@ -1,18 +1,31 @@
 package org.allmon.client.agent.aop;
 
+import org.allmon.client.agent.AgentContext;
 import org.allmon.client.agent.JavaCallAgent;
 import org.allmon.common.MetricMessage;
 import org.allmon.common.MetricMessageFactory;
 
 public class Advisor4Allmon {
+	
+	private AgentContext agentContext;
     
-    public void logBeforeMethodCall() {
+    public void setAgentContext(AgentContext agentContext) {
+		this.agentContext = agentContext;
+	}
+
+	public AgentContext getAgentContext() {
+		return agentContext;
+	}
+
+	public void logBeforeMethodCall() {
 //        param: AgentContext agentContext
 //        param: String classNameCalled, String methodNameCalled, String classNameCalling, String methodNameCalling
+		
+		System.out.println("Start AOP");
         MetricMessage metricMessage = MetricMessageFactory.createClassMessage(
                 this.getClass().getName(), "method", "", "", -1); // TODO review duration time param
         
-        JavaCallAgent agent = new JavaCallAgent(SpringHelloWorldController.agentContext, metricMessage);
+        JavaCallAgent agent = new JavaCallAgent(agentContext, metricMessage);
         agent.entryPoint();
     }
 
@@ -20,6 +33,8 @@ public class Advisor4Allmon {
 //        param: JavaCallAgent agent
 //        param: Exception exception
 //        agent.exitPoint();
+    	System.out.println("Stop AOP");
+    	agentContext.stop();
     }
     
 }
