0d3218b0f6a7102424593e912f6650c7957b3790
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-23 20:48:29 +0000

    Cleaning up project code

--- allmon-server-test/src/org/allmon/server/loader/LoadRawMetricLoadTest.java
+++ allmon-server-test/src/org/allmon/server/loader/LoadRawMetricLoadTest.java
@@ -1,6 +1,8 @@
 package org.allmon.server.loader;
 
 import org.allmon.common.AbstractLoadTest;
+import org.allmon.common.MetricMessageFactory;
+import org.allmon.common.MetricMessageWrapper;
 
 public class LoadRawMetricLoadTest extends AbstractLoadTest<LoadRawMetric, Object> {
 
@@ -8,24 +10,22 @@ public class LoadRawMetricLoadTest extends AbstractLoadTest<LoadRawMetric, Objec
     private final static long STARTING_TIME_MILLIS = 10;
 
     public void testMain() throws InterruptedException {
-        runLoadTest(THREADS_COUNT, 
-                STARTING_TIME_MILLIS, 1, 
-                2000, 10000);
+        runLoadTest(THREADS_COUNT, STARTING_TIME_MILLIS, 1, 2000, 10000);
     }
-    
+
     public LoadRawMetric initialize() {
         return new LoadRawMetric();
     }
-    
+
     public Object preCall(int thread, int iteration, LoadRawMetric loadRawMetric) {
-        RawMetric metric = new RawMetric();
-        String str = ">" + Math.random() + ">" + Math.random() + ">" + Math.random() + ">" + Math.random();
-        metric.setMetric(str);
-        loadRawMetric.storeMetric(metric);
-        return null;
+        MetricMessageWrapper metricMessageWrapper = new MetricMessageWrapper(
+                MetricMessageFactory.createClassMessage(
+                        "classNameCalled", "methodNameCalled", "classNameCalling", "methodNameCalling", 0));
+        loadRawMetric.storeMetric(metricMessageWrapper);
+        return metricMessageWrapper;
     }
-    
+
     public void postCall(Object agent) {
     }
-    	
+
 }
