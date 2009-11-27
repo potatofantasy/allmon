fd5da412bdfc3a880714c4c467396b53cc9a2432
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-27 12:35:48 +0000

    Changes to HttpUrlCallAgentFullResponseTimeStrategy preventing acquiring zero full time response time.

--- allmon-client/src/org/allmon/client/agent/HttpUrlCallAgentFullResponseTimeStrategy.java
+++ allmon-client/src/org/allmon/client/agent/HttpUrlCallAgentFullResponseTimeStrategy.java
@@ -3,8 +3,6 @@ package org.allmon.client.agent;
 import org.allmon.common.MetricMessage;
 import org.allmon.common.MetricMessageFactory;
 import org.allmon.common.MetricMessageWrapper;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * Acquired metrics values represent full time between connection 
@@ -15,15 +13,18 @@ import org.apache.commons.logging.LogFactory;
  */
 public class HttpUrlCallAgentFullResponseTimeStrategy extends HttpUrlCallAgentAbstractStrategy {
 
-    private static final Log logger = LogFactory.getLog(HttpUrlCallAgentFullResponseTimeStrategy.class);
+//    private static final Log logger = LogFactory.getLog(HttpUrlCallAgentFullResponseTimeStrategy.class);
     
     MetricMessageWrapper extractMetrics() {
         String foundPhrase = OutputParser.findFirst(bufferedReaderCallResponse, agent.searchPhrase);
 
+        // response time + transferring all content + parsing
+        long fullResponseTime = System.currentTimeMillis() - agent.stratTime;
+        
         double metricValue = 0;
         if (!foundPhrase.trim().equals("")) {
-            // response time + transferring all content + parsing
-            metricValue = System.currentTimeMillis() - agent.stratTime;
+            // if phrase can be found metric value is away greater than 0
+            metricValue = fullResponseTime > 0 ? fullResponseTime : 1; 
         }
 
         // create metrics object
