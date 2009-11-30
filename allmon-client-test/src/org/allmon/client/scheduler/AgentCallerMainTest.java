8150b5f2687288d05ed256ce2c88b55d0f604a4b
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-11-30 17:18:30 +0000

    Adding an optional parameter for scheduler which specifies a shift for active monitoring agents tasks - preventing running huge list of tasks at once.

--- allmon-client-test/src/org/allmon/client/scheduler/AgentCallerMainTest.java
+++ allmon-client-test/src/org/allmon/client/scheduler/AgentCallerMainTest.java
@@ -4,38 +4,23 @@ import junit.framework.TestCase;
 
 public class AgentCallerMainTest extends TestCase {
 
-	public void testMain() throws Exception {
-		//String [] args = {ShellCallAgent.class.getName(), ""};
-		//String [] args = {SimpleMetricMessageAgent.class.getName(), ""};
-	    //String [] args = {SonobuoyAgent.class.getName(), ""};
-	    String [] args = {
-    	    org.allmon.client.agent.HttpUrlCallAgent.class.getName(), 
-    	    "http://lontd01/AdminConsole/statuscheck/default.aspx/CheckStatus", 
-    	    "\"Success\":true", 
-    	    "application/json;charset=utf-8",
-    	    "{'componentChecker':'TTC.iTropics.ComponentCheckers.ITropicsServiceComponentChecker,TTC.iTropics.ComponentCheckers'}"};
-	        
-	    AgentCallerMain.main(args);
-	}
-
-//    public void testExecuteAgentTaskable() throws Exception {
-//        AgentContext agentContext = new AgentContext();
-//        UrlCallAgent agent = new UrlCallAgent(agentContext);
-//        String[] classParamsString = new String[]{"http://www.google.com", "\\d\\d\\d\\d"};        
-//        AgentCallerMain caller = new AgentCallerMain();
-//        caller.executeAgentTaskable(agent, classParamsString);
-//        agentContext.stop();
-//    }
-	
-	public static void main(String[] args) {
-        String [] args2 = {
-                org.allmon.client.agent.HttpUrlCallAgent.class.getName(), 
-                "http://lontd01/AdminConsole/statuscheck/default.aspx/CheckStatus", 
-                "\"Success\":true", 
-                "application/json;charset=utf-8",
-                "{'componentChecker':'TTC.iTropics.ComponentCheckers.ITropicsServiceComponentChecker,TTC.iTropics.ComponentCheckers'}"};
-                
-        AgentCallerMain.main(args2);
+    public void test4LocalBrokerCheck() throws Exception {
+        String [] args = {
+            org.allmon.client.agent.HttpUrlCallAgent.class.getName(), 
+            "10", // 10 seconds wait
+            org.allmon.client.agent.HttpUrlCallAgentFullResponseTimeStrategyTest.class.getName(), // setStrategy
+            "http://localhost:8161/admin/", //urlAddress
+            "Broker", // searchPhrase
+            "text/html", // contentType
+            "", // urlParameters
+            "localhost", // checkingHost
+            "local-broker-check", // checkName
+            "local-broker", // instanceName
+            "false", // useProxy
+            "POST" // requestMethod
+            };
+        
+        AgentCallerMain.main(args);
     }
-	
-} 	
+
+}
