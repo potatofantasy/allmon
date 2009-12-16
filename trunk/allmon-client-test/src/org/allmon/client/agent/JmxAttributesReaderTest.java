e017939b15a6867f06afeb73acacd4c3d31dcf73
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2009-12-16 21:27:35 +0000

    Working on JMX metrics acquisition

--- trunk/allmon-client-test/src/org/allmon/client/agent/JmxAttributesReaderTest.java
+++ trunk/allmon-client-test/src/org/allmon/client/agent/JmxAttributesReaderTest.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.allmon.client.agent.jmx.LocalVirtualMachineDescriptor;
 import org.allmon.client.agent.jmx.JmxAttributesReader;
 import org.allmon.client.agent.jmx.MBeanAttributeData;
 import org.allmon.common.AllmonPropertiesReader;
-
-import sun.tools.jconsole.LocalVirtualMachine;
 
 public class JmxAttributesReaderTest extends TestCase {
 
@@ -19,25 +18,25 @@ public class JmxAttributesReaderTest extends TestCase {
     public void testLVMList() throws Exception {
         JmxAttributesReader jmxReader = new JmxAttributesReader();
 
-        List<LocalVirtualMachine> lvmList = jmxReader.getLocalVirtualMachine(this.getClass().getName(), false);
+        List<LocalVirtualMachineDescriptor> lvmList = jmxReader.getLocalVirtualMachine(this.getClass().getName(), false);
         assertEquals(1, lvmList.size());
 
-        LocalVirtualMachine lvm = lvmList.get(0);
-        assertNotSame(0, lvm.vmid());
+        LocalVirtualMachineDescriptor lvm = lvmList.get(0);
+        assertNotSame(0, lvm.getVMid());
     }
 	
 	public void testMBeansAttributes() throws Exception {
 		JmxAttributesReader jmxReader = new JmxAttributesReader();
-
-        List<LocalVirtualMachine> lvmList = jmxReader.getLocalVirtualMachine(this.getClass().getName(), false);
+		
+        List<LocalVirtualMachineDescriptor> lvmList = jmxReader.getLocalVirtualMachine(this.getClass().getName(), false);
         assertTrue(lvmList.size() > 0);
-
-        LocalVirtualMachine lvm = lvmList.get(0);
-        assertNotSame(0, lvm.vmid());
-
+        
+        LocalVirtualMachineDescriptor lvm = lvmList.get(0);
+        assertNotSame(0, lvm.getVMid());
+        
         List<MBeanAttributeData> attributeDataList = jmxReader.getMBeansAttributesData(lvm, "", false);
         assertTrue(attributeDataList.size() > 0);
-
+        
 		List<MBeanAttributeData> attributeDataListMemoryUsed = 
             jmxReader.getMBeansAttributesData(lvm, ".*java.lang:type=Memory:.*HeapMemoryUsage/used", false);
         assertEquals(2, attributeDataListMemoryUsed.size()); 
@@ -47,25 +46,24 @@ public class JmxAttributesReaderTest extends TestCase {
         List<MBeanAttributeData> attributeDataListGc = 
             jmxReader.getMBeansAttributesData(lvm, "sun.management.GarbageCollector", false);
         assertTrue(attributeDataListGc.size() > 0);
-
+        
         List<MBeanAttributeData> attributeDataListMemory = 
             jmxReader.getMBeansAttributesData(lvm, "sun.management.Memory", false); // MemoryPool
         assertTrue(attributeDataListMemory.size() > 0);
-
+        
         List<MBeanAttributeData> attributeDataListOs = 
             jmxReader.getMBeansAttributesData(lvm, "sun.management.OperatingSystem", false);
         assertTrue(attributeDataListOs.size() > 0);
-
     }
     
     public void testMBeansAttributesLVMListAllOs() throws Exception {
         JmxAttributesReader jmxReader = new JmxAttributesReader();
 
-        List<LocalVirtualMachine> lvmList = jmxReader.getLocalVirtualMachine("", false);
+        List<LocalVirtualMachineDescriptor> lvmList = jmxReader.getLocalVirtualMachine("", false);
         assertTrue(lvmList.size() > 0);
         
-        for (LocalVirtualMachine lvm : lvmList) {
-            assertTrue(lvm.vmid() > 0);
+        for (LocalVirtualMachineDescriptor lvm : lvmList) {
+            assertTrue(lvm.getVMid() > 0);
             try {
                 List<MBeanAttributeData> attributeDataListOs = 
                     jmxReader.getMBeansAttributesData(lvm, "sun.management.OperatingSystem", false);
