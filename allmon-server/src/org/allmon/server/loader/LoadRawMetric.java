35099bc79bca85cfca9d57f4b0f1fc300dc5cc20
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2010-01-06 21:47:49 +0000

    Improving loading process by creating many concurrent threads using db connection pool to store more data in a period of time

--- allmon-server/src/org/allmon/server/loader/LoadRawMetric.java
+++ allmon-server/src/org/allmon/server/loader/LoadRawMetric.java
@@ -14,36 +14,29 @@ public class LoadRawMetric {
     private static final Log logger = LogFactory.getLog(LoadRawMetric.class);
     
     private static final ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(new String[] { "org/allmon/server/loader/spring-hibernate.xml" });
-    //private ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(new String[] { "org/allmon/loader/spring-hibernate.xml" });
     
-    // TODO to improve performance:
-    // TODO the loading process should be split in to many concurrently running threads
-    // TODO use batch loading!
     public void storeMetric(MetricMessageWrapper metricMessageWrapper) {
         RawMetric2DAOImpl rawMetric2DAOImpl = (RawMetric2DAOImpl)appContext.getBean("rawMetric2DAOTarget");
         
         // convert metricMessageWrapper and load metrics data to the database
         MetricMessageConverter messageConverter = new MetricMessageConverter();
         RawMetric2[] rawMetricTab = messageConverter.convert(metricMessageWrapper);
-//        for (int i = 0; i < rawMetricTab.length; i++) {
-//            try {
-//                rawMetric2DAOImpl.addMetric(rawMetricTab[i]);
-//            } catch (Exception ex) {
-//                logger.error(ex.getMessage(), ex);
-//            }
-//		  }
-        // Runs loading process of all metrics data in the table in many threads
-        RawMetricConcurrentLoader concurrentLoader = new RawMetricConcurrentLoader(
-                rawMetric2DAOImpl, rawMetricTab, 10); // TODO add external parameter
+        
+        // run loading process of all metrics data in the table in many threads
+        // for small amount of data to load run loading process in separate but single thread only
+        int concurrentThreadsCount = rawMetric2DAOImpl.getConcurrentThreadsCount(rawMetricTab.length);
+        final RawMetricConcurrentLoader concurrentLoader = new RawMetricConcurrentLoader(
+                    rawMetric2DAOImpl, rawMetricTab, concurrentThreadsCount);
+        
         try {
             concurrentLoader.runLoad();
         } catch (InterruptedException ex) {
             logger.error(ex.getMessage(), ex);
         }
-                
-        logger.debug(">>>>>>>>>>>>>>>> Metrics stored: " + metricMessageWrapper.toString());
+//        logger.debug(">>>>>>>>>>>>>>>> Metrics stored: " + metricMessageWrapper.toString());
     }
     
+    // TODO add synchronisation mechanism preventing running many allmetrics loading processes!
     public void loadAllmetric() {
         long t0 = System.currentTimeMillis();
         RawMetricLoadToAllmetricDAOImpl loadToAllmetricDAOImpl = 
--- allmon-server/src/org/allmon/server/loader/RawMetric2DAOImpl.java
+++ allmon-server/src/org/allmon/server/loader/RawMetric2DAOImpl.java
@@ -4,8 +4,48 @@ import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 public class RawMetric2DAOImpl extends HibernateDaoSupport implements RawMetric2DAO {
 
+    private int maxConcurrentThreadsCount = 1;
+    
+    private int preferedMetricsCountPerThread = 10;
+    
     public void addMetric(RawMetric2 metric) {
         getHibernateTemplate().save(metric);
+    }
+
+    public int getMaxConcurrentThreadsCount() {
+        return maxConcurrentThreadsCount;
+    }
+
+    public void setMaxConcurrentThreadsCount(int maxConcurrentThreadsCount) {
+        this.maxConcurrentThreadsCount = maxConcurrentThreadsCount;
+    }
+
+    public int getPreferedMetricsCountPerThread() {
+        return preferedMetricsCountPerThread;
+    }
+
+    public void setPreferedMetricsCountPerThread(int preferedMetricsCountPerThread) {
+        this.preferedMetricsCountPerThread = preferedMetricsCountPerThread;
+    }
+    
+    /**
+     * 
+     * <li> For not big amount of data to load method will return smaller than maximum 
+     * count of concurrent threads, according to formula: <i>metricsCount / preferedMetricsCountPerThread + 1</i>.
+     * <li> If metrics count is smaller than maximum count of concurrent threads then 
+     * loading process will be executed in a separate but single thread only.
+     * <li> In other cases result will be equal to maximum count of concurrent threads.
+     * 
+     * @param metricsCount
+     * @return count of threads which should run loading process for set metricCount parameter
+     */
+    public int getConcurrentThreadsCount(int metricsCount) {
+        if (metricsCount < maxConcurrentThreadsCount) {
+            return 1;
+        } else if (metricsCount / preferedMetricsCountPerThread < maxConcurrentThreadsCount) {
+            return metricsCount / preferedMetricsCountPerThread + 1;
+        }
+        return maxConcurrentThreadsCount;
     }
 
 }
