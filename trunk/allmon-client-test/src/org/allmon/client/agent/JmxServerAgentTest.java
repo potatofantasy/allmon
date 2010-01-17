6588c57743bc12c203ae0761fb1d84af3bdc0d4e
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2010-01-17 01:21:32 +0000

    Adding example of JMX agent definition, for: JDK pools an OS memory allocations, GC stats, thread count, class loading stats

--- trunk/allmon-client-test/src/org/allmon/client/agent/JmxServerAgentTest.java
+++ trunk/allmon-client-test/src/org/allmon/client/agent/JmxServerAgentTest.java
@@ -35,13 +35,34 @@ public class JmxServerAgentTest extends TestCase {
         try {
             JmxServerAgent agent = new JmxServerAgent(agentContext);
             agent.setParameters(new String[]{
-                    ".*activemq.*", //".*jboss.*", //".*AgentAggregatorMain.*",
+                    // active mq
+                    //".*activemq.*",
                     //JDK pools an OS memory allocations, GC stats, thread count, class loading
-                    "(.*java.lang:type=Memory.*used.*)|(.*java.lang:type=OperatingSystem.*)|(.*java.lang:type=GarbageCollector.*)|(.*java.lang:type=Threading.*ThreadCount.*)|(.*java.lang:type=ClassLoading.*)",
-                    //".*jboss.system:type=ServerInfo.*",
+                    //"(.*java.lang:type=Memory.*used.*)|(.*java.lang:type=OperatingSystem.*)|(.*java.lang:type=GarbageCollector.*)|(.*java.lang:type=Threading.*ThreadCount.*)|(.*java.lang:type=ClassLoading.*)",
                     //".*java.lang:type=Threading:CurrentThreadCpuTime" //"java.lang:type=Threading"
                     //".*java.lang:type=Runtime.*"
-                    //java.lang:type=Compilation:
+                    
+                    // allmon
+                    //".*allmon.*", //".*AgentAggregatorMain.*",
+                    //"(.*java.lang:type=Memory.*used.*)|(.*java.lang:type=OperatingSystem.*)|(.*java.lang:type=GarbageCollector.*)|(.*java.lang:type=Threading.*ThreadCount.*)|(.*java.lang:type=ClassLoading.*)"
+                    
+                    //jboss specific
+                    //".*jboss.*", // all jboss based metrics
+                    //".*jboss.j2ee:service=EJB,plugin=pool,.*:CurrentSize.*", // ejb pools
+                    //".*jboss.jca:service=CachedConnectionManager:InUseConnections.*",
+                    //".*jboss.jca:service=JCAMetaDataRepository,name=DefaultJCAMetaDataRepository:ManagedConnectionFactoryCount.*",
+                    //".*jboss.jca:service=ManagedConnectionPool,name=.*:.*Connection.*Count.*" //ex: ConnectionCount, InUseConnectionCount, ConnectionCreatedCount, ConnectionDestroyedCount
+                    //".*jboss.messaging.destination:service=Queue,name=.*:.*", // ex: ConsumerCount, ScheduledMessageCount, RedeliveryDelay, FullSize, 
+                    //".*jboss.system:type=ServerInfo.*", // ex: ActiveThreadCount, AvailableProcessors, FreeMemory, MaxMemory, TotalMemory
+                    //".*jboss.web:j2eeType=Servlet,name=.*J2EEServer=.*Count.*" // ex: requestCount, errorCount
+                    //".*jboss.web:j2eeType=Servlet,name=.*J2EEServer=.*Time.*" // ex: loadTime, processingTime, classLoadTime
+                    //".*jboss.web:type=Cache,host=.*,path=.*", // ex: hitsCount, accessCount, cacheSize, spareNotFoundEntries, ...
+                    //".*jboss.web:type=Manager,path=.*,host.*:activeSessions.*", // ex: activeSessions, expiredSessions, sessionCounter, rejectedSessions
+                    //".*jboss:service=invoker,type=pooled:.*", // ex: CurrentClientPoolSize, CurrentThreadPoolSize, ClientRetryCount
+                    //".*jboss:service=TransactionManager:.*Count.*", // ex: CommitCount, RunningTransactionCount, HeuristicCount, RollbackCount, TimedoutCount
+                    ".*JMImplementation:type=MBeanRegistry:Size*", // ex: MBeanRegistry:Size
+                    
+                    
             });
             //agent.execute(); // would collect metrics and sent the metrics messages to the broker
             agent.decodeAgentTaskableParams();
