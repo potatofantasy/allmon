55bdd4eb6016d6fbc2d4a20a56c29f5fead9082a
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2010-02-17 19:48:12 +0000

    Adding better support for parameters and exceptions acquisition

--- trunk/allmon-client-test/src/org/allmon/client/agent/JavaCallAgentLoadTest.java
+++ trunk/allmon-client-test/src/org/allmon/client/agent/JavaCallAgentLoadTest.java
@@ -20,7 +20,7 @@ public class JavaCallAgentLoadTest extends AbstractLoadTest<Object, JavaCallAgen
     private final static long THREADS_COUNT = 10;
     private final static long STARTING_TIME_MILLIS = 1 * 1000; // rump-up 1 sec
     private final static long SUBSEQUENT_CALLS_IN_THREAD_SLEEP_MAX = 20; // 25ms * 5000x = ~125sec
-    private final static long SUBSEQUENT_CALLS_IN_THREAD = 10000; //300000; //20000; //10000; //5000;
+    private final static long SUBSEQUENT_CALLS_IN_THREAD = 10; //300000; //20000; //10000; //5000;
     // soak test - around 60min
 //    private final static long STARTING_TIME_MILLIS = 1 * 60 * 1000; // rump-up 1 min
 //    private final static long SUBSEQUENT_CALLS_IN_THREAD_SLEEP_MAX = 20;
@@ -41,10 +41,12 @@ public class JavaCallAgentLoadTest extends AbstractLoadTest<Object, JavaCallAgen
     }
     
     public JavaCallAgent preCall(int thread, int iteration, Object initParameters) {
-//        MetricMessage metricMessage = MetricMessageFactory4Test.createClassMessage(
-//                "className" + iteration, "methodName", "classNameX", "methodNameX", 1);
-        MetricMessage metricMessage = MetricMessageFactory4Test.createActionClassMessage(
-                this.getClass().getName(), "user", "webSessionId", null);
+        MetricMessage metricMessage = MetricMessageFactory4Test.createClassMessage(
+                "className" + iteration, "methodName", "classNameX", "methodNameX", 1);
+        metricMessage.setParameters(new String[]{"string1", "string2"});
+        
+//        MetricMessage metricMessage = MetricMessageFactory4Test.createActionClassMessage(
+//                this.getClass().getName(), "user", "webSessionId", null);
         
         JavaCallAgent agent = new JavaCallAgent(agentContext, metricMessage);
         agent.entryPoint();
