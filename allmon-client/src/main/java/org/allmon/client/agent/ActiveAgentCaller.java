5416d2aeb171967cf8537b7df553824dd07e7ac9
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2010-06-08 12:09:55 +0000

    Removing cron4j implementations - use of quartz scheduler with spring

---
+++ allmon-client/src/main/java/org/allmon/client/agent/ActiveAgentCaller.java
@@ -0,0 +1,41 @@
+package org.allmon.client.agent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.quartz.JobExecutionContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class ActiveAgentCaller extends QuartzJobBean {
+
+	private static final Log logger = LogFactory.getLog(ActiveAgentCaller.class);
+	
+	private String activeAgentClass;
+	private ActiveAgentParameters parameters;
+	private ActiveAgent activeAgent;
+	
+
+	public void setActiveAgentClass(String activeAgentClass) {
+		this.activeAgentClass = activeAgentClass;
+	}
+
+	public void setParameters(ActiveAgentParameters parameters) {
+		this.parameters = parameters;
+	}
+	
+	public void setActiveAgent(ActiveAgent activeAgent) {
+		this.activeAgent = activeAgent;
+	}
+	
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) {
+		System.out.println("Call active agent: " + activeAgentClass + " - " + parameters.getName()); // TODO move to log4j
+		
+		System.out.println("activeAgent.execute() " + activeAgent.getClass() + "...");
+				
+		logger.debug("Execution : " + activeAgent.getClass() + ".execute() ...");
+		activeAgent.execute();
+		logger.debug("Execution : " + activeAgent.getClass() + ".execute() finished");
+		
+	}
+
+}
---
+++ allmon-client/src/test/java/org/allmon/client/agent/ActiveAgentParameters.java
@@ -0,0 +1,15 @@
+package org.allmon.client.agent;
+
+public class ActiveAgentParameters {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
