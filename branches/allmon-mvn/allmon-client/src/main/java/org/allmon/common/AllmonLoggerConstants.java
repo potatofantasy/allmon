bb57ce61044c56166539760b63d647cd20ffba20
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2010-08-12 17:12:53 +0000

    Deleting sources moved to allmon-common from project

--- branches/allmon-mvn/allmon-client/src/main/java/org/allmon/common/AllmonLoggerConstants.java
+++
@@ -1,8 +0,0 @@
-package org.allmon.common;
-
-public class AllmonLoggerConstants {
-
-    public static final String ENTERED = " Entered ";
-    public static final String EXITED = " Exited ";
-    
-}
