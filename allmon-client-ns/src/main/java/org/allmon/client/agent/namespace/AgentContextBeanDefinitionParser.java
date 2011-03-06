2d31cd8f56f9d78ba304bf07b8aea3ef236a9b67
sikora.t <sikora.t@9e2eac6c-fd4c-11dd-a320-db7d8e7a638f>
2011-03-06 21:39:36 +0000

    Restructuring client-side projects. Moving common for both agents spring namespace mechanisms to allmon-client-ns module.

---
+++ allmon-client-ns/src/main/java/org/allmon/client/agent/namespace/AgentContextBeanDefinitionParser.java
@@ -0,0 +1,17 @@
+package org.allmon.client.agent.namespace;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+public class AgentContextBeanDefinitionParser implements BeanDefinitionParser {
+
+	public BeanDefinition parse(Element element, ParserContext parserContext) {
+		
+		// FIXME add parsing implementation 
+		
+		return null;
+	}
+	
+}
